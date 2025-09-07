e1b26483f6b7999eb62483d8e8b78b0e1fc00993
Liam Bowen <LiamBowen@gmail.com>
2025-09-07 09:03:39 +0000

    This index will likely be dropped due to its column being dropped...

--- migrations/20250907000001_airworthiness_classes_to_enum.up.sql
+++ migrations/20250907000001_airworthiness_classes_to_enum.up.sql
@@ -6,7 +6,7 @@
 -- 1. Create the enum type with the current values
 CREATE TYPE airworthiness_class AS ENUM (
     'Standard',
-    'Limited', 
+    'Limited',
     'Restricted',
     'Experimental',
     'Provisional',
@@ -17,12 +17,12 @@ CREATE TYPE airworthiness_class AS ENUM (
 );
 
 -- 2. Add new column with enum type to aircraft_registrations table
-ALTER TABLE aircraft_registrations 
+ALTER TABLE aircraft_registrations
 ADD COLUMN airworthiness_class airworthiness_class;
 
 -- 3. Update the new column based on current foreign key relationships
-UPDATE aircraft_registrations 
-SET airworthiness_class = CASE 
+UPDATE aircraft_registrations
+SET airworthiness_class = CASE
     WHEN ac.code = '1' THEN 'Standard'::airworthiness_class
     WHEN ac.code = '2' THEN 'Limited'::airworthiness_class
     WHEN ac.code = '3' THEN 'Restricted'::airworthiness_class
@@ -37,16 +37,16 @@ FROM airworthiness_classes ac
 WHERE aircraft_registrations.airworthiness_class_code = ac.code;
 
 -- 4. Drop the foreign key constraint
-ALTER TABLE aircraft_registrations 
+ALTER TABLE aircraft_registrations
 DROP CONSTRAINT aircraft_registrations_airworthiness_class_code_fkey;
 
 -- 5. Drop the old column
-ALTER TABLE aircraft_registrations 
+ALTER TABLE aircraft_registrations
 DROP COLUMN airworthiness_class_code;
 
 -- 6. Drop the airworthiness_classes table
 DROP TABLE airworthiness_classes;
 
 -- 7. Update the index to use the new column
-DROP INDEX aircraft_registrations_aw_class_idx;
+DROP INDEX IF EXISTS aircraft_registrations_aw_class_idx;
 CREATE INDEX aircraft_registrations_aw_class_idx ON aircraft_registrations (airworthiness_class, type_aircraft_code);
