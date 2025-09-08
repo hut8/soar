8090580a1c5d5f1daa27040bb6666397a45caddb
Liam Bowen <LiamBowen@gmail.com>
2025-09-08 02:26:47 +0000

    use es modules

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,26 +1,24 @@
-const { skeleton } = require('@skeletonlabs/tw-plugin');
-const { join } = require('path');
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from 'path';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-	darkMode: 'class',
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
-	],
-	theme: {
-		extend: {}
-	},
-	plugins: [
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'skeleton',
-						enhancements: true
-					}
-				]
-			}
-		})
-	]
+export const darkMode = 'class';
+export const content = [
+    './src/**/*.{html,js,svelte,ts}',
+    join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+];
+export const theme = {
+    extend: {}
 };
+export const plugins = [
+    skeleton({
+        themes: {
+            preset: [
+                {
+                    name: 'skeleton',
+                    enhancements: true
+                }
+            ]
+        }
+    })
+];
