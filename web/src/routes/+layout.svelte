1c366b196c6c8abc86b57ad12f94265a4da70f0d
Liam Bowen <LiamBowen@gmail.com>
2025-11-04 05:50:54 +0000

    fix: add grid-flow-col to AppBar.Toolbar for horizontal layout

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -109,7 +109,7 @@
 <div class="flex h-full min-h-screen flex-col">
 	<AppBar class="relative z-[70] bg-orange-400 dark:bg-orange-900">
 		<LoadingBar />
-		<AppBar.Toolbar>
+		<AppBar.Toolbar class="grid-flow-col">
 			<AppBar.Lead>
 				<a
 					href={base}
