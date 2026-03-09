8ef1213a59b1952aa50543e8e76e53e7ea66fb80
Liam Bowen <liam@supervillains.io>
2026-03-09 23:49:56 +0000

    Fix CI: merge main, rename migration, keep software column
    - Merge main to pick up new migrations on the same date
    - Rename migration to avoid timestamp collision
      (2026-03-09-000000 -> 2026-03-09-200000)
    - Keep software column in DB (don't drop it) to satisfy diesel-guard
    - Add protocols column alongside software; software is unused but
      retained to avoid unsafe DROP COLUMN in this migration

--- migrations/2026-03-09-000000-0000_replace_software_with_protocols/down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE receivers ADD COLUMN software TEXT;
-UPDATE receivers SET software = protocols[1] WHERE protocols IS NOT NULL AND array_length(protocols, 1) > 0;
-ALTER TABLE receivers DROP COLUMN protocols;
--- migrations/2026-03-09-200000-0000_add_protocols_to_receivers/up.sql
+++ migrations/2026-03-09-200000-0000_add_protocols_to_receivers/up.sql
@@ -1,3 +1,2 @@
 ALTER TABLE receivers ADD COLUMN protocols TEXT[];
 UPDATE receivers SET protocols = ARRAY[software] WHERE software IS NOT NULL;
-ALTER TABLE receivers DROP COLUMN software;
